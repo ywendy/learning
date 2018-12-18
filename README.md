fad8586abcedb1782e9300a9584423cf929cc2f0
yangpengbing <1244163720@qq.com>
2018-12-18 23:03:54 +0800

    编写ArrayList源码

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 学习记录 learning
+
+## learning-sourcecode jdk源码学习
---
+++ learning-sourcecode/src/test/java/com/ypb/sourcecode/ArrayListTest.java
@@ -0,0 +1,65 @@
+package com.ypb.sourcecode;
+
+import org.junit.Test;
+
+import java.util.List;
+/**
+ * @className ArrayListTest
+ * @description 测试ArrayList
+ * @author yangpengbing
+ * @date 22:33 2018/12/18
+ * @version 1.0.0
+ */
+public class ArrayListTest {
+
+    @Test
+    public void size() throws Exception {
+        List<String> strs = new ArrayList<>();
+        System.out.println(strs.size());
+    }
+
+    @Test
+    public void removeIf() throws Exception {
+    }
+
+    @Test
+    public void replaceAll() throws Exception {
+    }
+
+    @Test
+    public void sort() throws Exception {
+    }
+
+    @Test
+    public void forEach() throws Exception {
+    }
+
+    @Test
+    public void spliterator() throws Exception {
+    }
+
+    @Test
+    public void stream() throws Exception {
+    }
+
+    @Test
+    public void parallelStream() throws Exception {
+    }
+
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void add() throws Exception {
+    }
+
+    @Test
+    public void get1() throws Exception {
+    }
+
+    @Test
+    public void set() throws Exception {
+    }
+
+}
