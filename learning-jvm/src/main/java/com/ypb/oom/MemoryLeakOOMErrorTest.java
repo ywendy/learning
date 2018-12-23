cb8f3208dcd85edd9fe4355190792062005e3695
yangpengbing <1244163720@qq.com>
2018-12-23 22:28:07 +0800

    test memory leak oom

---
+++ learning-jvm/src/main/java/com/ypb/oom/MemoryLeakOOMErrorTest.java
@@ -0,0 +1,52 @@
+package com.ypb.oom;
+
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+/**
+ * @className MemoryLeakOOMErrorTest
+ * @description 内存泄漏导致的内存溢出
+ * @author yangpengbing
+ * @date 17:47 2018/12/23
+ * @version 1.0.0
+ */
+public class MemoryLeakOOMErrorTest {
+
+    public static void main(String[] args) {
+        Map<Key, String> map = Maps.newHashMap();
+        while (map.size() >= 0) {
+            for (int i = 0; i < 10000; i++) {
+                Key key = new Key(i);
+                if (map.containsKey(key)) {
+                    continue;
+                }
+
+                map.put(key, "number: " + i);
+            }
+        }
+    }
+
+    private static class Key {
+        private final Integer id;
+
+        public Key(Integer id) {
+            this.id = id;
+        }
+
+        @Override
+        public int hashCode() {
+            return id.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            boolean flag = Boolean.FALSE;
+            if (obj instanceof Key) {
+                flag = ((Key) obj).id.equals(this.id);
+            }
+
+            return flag;
+        }
+    }
+}
