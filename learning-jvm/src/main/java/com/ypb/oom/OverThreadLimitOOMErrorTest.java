e25f26e041f21fab518273399ebb5ec6a5f2ea3c
yangpengbing <1244163720@qq.com>
2018-12-23 22:42:56 +0800

    垃圾收集超时导致oom 测试

--- learning-jvm/src/main/java/com/ypb/oom/OverThreadLimitOOMErrorTest.java
+++ learning-jvm/src/main/java/com/ypb/oom/OverThreadLimitOOMErrorTest.java
@@ -1,4 +1,23 @@
 package com.ypb.oom;
 
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * @className OverThreadLimitOOMErrorTest
+ * @description 垃圾回收超时导致的内存溢出(-Xmx32m -Xms32m -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -verbose:gc -Xloggc:E:/log.gc -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=D:/dump/dump.hprof)
+ * @author yangpengbing
+ * @date 22:31 2018/12/23
+ * @version 1.0.0
+ */
 public class OverThreadLimitOOMErrorTest {
+
+    public static void main(String[] args) {
+        Map map = System.getProperties();
+        Random r = new Random();
+        while (map.size() >= 0) {
+            int i = r.nextInt();
+            map.put(i, "ypb" + i);
+        }
+    }
 }
