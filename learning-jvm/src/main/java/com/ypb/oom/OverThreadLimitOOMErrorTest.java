1a712db58a41ea281dcaf971150d145069385154
yangpengbing <yangpengbing@hualala.com>
2018-12-24 18:32:56 +0800

    GC overhead limit exceeded

--- learning-jvm/src/main/java/com/ypb/oom/OverThreadLimitOOMErrorTest.java
+++ learning-jvm/src/main/java/com/ypb/oom/OverThreadLimitOOMErrorTest.java
@@ -1,23 +1,28 @@
 package com.ypb.oom;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 import java.util.Map;
 import java.util.Random;
 
 /**
  * @className OverThreadLimitOOMErrorTest
- * @description 垃圾回收超时导致的内存溢出(-Xmx32m -Xms32m -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -verbose:gc -Xloggc:E:/log.gc -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=D:/dump/dump.hprof)
+ * @description 垃圾回收超时导致的内存溢出(-Xmx32m -Xms32m -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -verbose:gc -Xloggc: E:/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=D:/dump/dump.hprof)
+ * https://blog.csdn.net/renfufei/article/details/77585294
  * @author yangpengbing
  * @date 22:31 2018/12/23
  * @version 1.0.0
  */
 public class OverThreadLimitOOMErrorTest {
 
-    public static void main(String[] args) {
-        Map map = System.getProperties();
-        Random r = new Random();
-        while (map.size() >= 0) {
-            int i = r.nextInt();
-            map.put(i, "ypb" + i);
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
+		System.out.println(bean.getInputArguments());
+
+		Map map = System.getProperties();
+		Random r = new Random();
+		while (map.size() >= 0) {
+			map.put(r.nextInt(), "ypb");
+		}
+	}
 }
