fbe54902b2c95fc2eab85cafd71377da3578b023
yangpengbing <1244163720@qq.com>
2019-03-11 22:54:47 +0800

    spring常用注解

--- learning-spring/src/main/java/com/ypb/spring/annotation/package-info.java
+++ learning-spring/src/main/java/com/ypb/spring/annotation/package-info.java
@@ -2,3 +2,15 @@ package com.ypb.spring.annotation;
 
 // 学习spring注解驱动
 // spring开发中常用的注解
+// controller
+// service
+// reposetry
+// component
+// configuration
+// bean
+// componentScan(自定义typeFilter)
+// scope
+// lazy
+// conditional
+// import
+// FactoryBean
