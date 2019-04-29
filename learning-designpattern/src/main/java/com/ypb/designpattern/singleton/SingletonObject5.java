b066d31bd658ae2d8f5aec5fbe04c604a56f3b36
yangpengbing <yangpengbing@hualala.com>
2019-04-29 17:33:58 +0800

    create singleton pattern模式

--- learning-designpattern/src/main/java/com/ypb/designpattern/singleton/SingletonObject5.java
+++ learning-designpattern/src/main/java/com/ypb/designpattern/singleton/SingletonObject5.java
@@ -1,5 +1,11 @@
 package com.ypb.designpattern.singleton;
-
+/**
+ * @ClassName: SingletonObject5
+ * @Description: {@link com.ypb.concurrent.stage2.chapter1.SingletonObject5}
+ * @author yangpengbing
+ * @date 2019-04-29-14:10
+ * @version V1.0.0
+ */
 public class SingletonObject5 {
 
 }
