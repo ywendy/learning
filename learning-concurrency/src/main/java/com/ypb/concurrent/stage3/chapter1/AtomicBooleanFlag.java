4860dbfc783e7eb973b044121c673d39dbf8e6ef
yangpengbing <1244163720@qq.com>
2019-05-14 23:14:40 +0800

    AtomicLong和AtomicBoolean的使用

--- learning-concurrency/src/main/java/com/ypb/concurrent/stage3/chapter1/AtomicBooleanFlag.java
+++ learning-concurrency/src/main/java/com/ypb/concurrent/stage3/chapter1/AtomicBooleanFlag.java
@@ -1,4 +1,74 @@
 package com.ypb.concurrent.stage3.chapter1;
 
+import java.util.concurrent.TimeUnit;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class AtomicBooleanFlag {
+
+    //    private static final AtomicBoolean ab = new AtomicBoolean(true);
+    private static final VolatileBoolean ab = new VolatileBoolean();
+
+    private static boolean flag = true;
+
+    public static void main(String[] args) throws InterruptedException {
+//        test1();
+
+        test2();
+    }
+
+    private static void test2() throws InterruptedException {
+        new Thread(AtomicBooleanFlag::runTask2).start();
+
+        TimeUnit.MILLISECONDS.sleep(5000L);
+        new Thread(()-> flag = false).start();
+    }
+
+    private static void runTask2() {
+        while (flag) {
+
+//            try {
+//                TimeUnit.MILLISECONDS.sleep(1000);
+                System.out.println("I am woking..");
+//            } catch (InterruptedException e) {
+//                log.debug(e.getMessage(), e);
+//            }
+        }
+
+        System.out.println("I am finish..");
+    }
+
+    private static void test1() throws InterruptedException {
+        new Thread(AtomicBooleanFlag::runTask).start();
+
+        TimeUnit.MILLISECONDS.sleep(5000);
+
+        ab.set(false);
+    }
+
+    private static void runTask() {
+        while (ab.get()) {
+            try {
+                Thread.sleep(1000);
+                System.out.println("I am working..");
+            } catch (InterruptedException e) {
+                log.debug(e.getMessage(), e);
+            }
+        }
+
+        System.out.println("I am finish..");
+    }
+
+    private static class VolatileBoolean {
+        private volatile boolean falg = true;
+
+        public boolean get() {
+            return falg;
+        }
+
+        public void set(boolean falg) {
+            this.falg = falg;
+        }
+    }
 }
--- learning-concurrency/src/test/java/com/ypb/concurrent/stage3/chapter1/AtomicBooleanTest.java
+++ learning-concurrency/src/test/java/com/ypb/concurrent/stage3/chapter1/AtomicBooleanTest.java
@@ -1,4 +1,52 @@
 package com.ypb.concurrent.stage3.chapter1;
 
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class AtomicBooleanTest {
+    
+    @Test
+    public void testCreateWithOutArgument(){
+        AtomicBoolean ab = new AtomicBoolean();
+
+        assertFalse(ab.get());
+    }
+
+    @Test
+    public void testCreateWithArgument() {
+        AtomicBoolean ab = new AtomicBoolean(true);
+
+        assertTrue(ab.get());
+    }
+
+    @Test
+    public void testGetAndSet() {
+        AtomicBoolean ab = new AtomicBoolean(Boolean.TRUE);
+        boolean value = ab.getAndSet(Boolean.FALSE);
+
+        assertTrue(value);
+        assertFalse(ab.get());
+    }
+
+    @Test
+    public void testCompareAndSet(){
+        AtomicBoolean ab = new AtomicBoolean(Boolean.TRUE);
+        boolean success = ab.compareAndSet(Boolean.TRUE, Boolean.FALSE);
+
+        assertTrue(success);
+        assertFalse(ab.get());
+    }
+
+    @Test
+    public void testCompareAndSetFailed() {
+        AtomicBoolean ab = new AtomicBoolean(Boolean.TRUE);
+        boolean success = ab.compareAndSet(Boolean.FALSE, Boolean.TRUE);
+
+        assertFalse(success);
+        assertTrue(ab.get());
+    }
 }
