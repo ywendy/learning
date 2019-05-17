67ea236b6cbc097048f6afad1c31a2383ca7a40a
yangpengbing <yangpengbing@hualala.com>
2019-05-17 14:43:26 +0800

    时间轮修复

--- learning-timer/src/test/java/com/ypb/timer/TimerTest.java
+++ learning-timer/src/test/java/com/ypb/timer/TimerTest.java
@@ -1,17 +1,28 @@
 package com.ypb.timer;
 
+import org.junit.Test;
+
 public class TimerTest {
 
-	public static void main(String[] args) {
+	@Test
+	public void testTimer() {
 		Timer timer = Timer.getInstance();
 
-		Integer times = 100;
+		Integer times = 1000;
 		for (Integer i = 0; i < times; i++) {
 			long delayMs = (long) (Math.random() * 100000L);
 
 			Integer finalI = i;
-			TimedTask task = new TimedTask(delayMs, () -> System.out.println(finalI));
+			TimedTask task = new TimedTask(delayMs, () -> runnableTask(finalI));
 			timer.addTask(task);
 		}
+
+		while (true) {
+
+		}
+	}
+
+	private void runnableTask(final Integer finalI) {
+		System.out.println(Thread.currentThread().getName() + " execute " + finalI);
 	}
 }
